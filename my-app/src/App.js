b0cd3847da08c38d3603b6297a73b5f14d1d0a98
thdwlsgus0 <thdwlsgus0@naver.com>
2024-01-05 23:10:21 +0900

    feat: infiniteQuery 적용

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,40 @@
 import "./App.css";
-import { useQuery } from "react-query";
+import { useInfiniteQuery } from "react-query";
 
 function App() {
-  const fetchRepositories = async () => {
+  const LIMIT = 10;
+
+  const fetchRepositories = async (page) => {
     const response = await fetch(
-      "https://api.github.com/search/repositories?q=topic:react"
+      `https://api.github.com/search/repositories?q=topic:react&per_page=${LIMIT}&page=${page}`
     );
 
     return response.json();
   };
 
-  const { data, isSuccess } = useQuery("repos", fetchRepositories);
+  const { data, isSuccess, hasNextPage, fetchNextPage, isFetchingNextPage } =
+    useInfiniteQuery(
+      "repos",
+      ({ pageParam = 1 }) => fetchRepositories(pageParam),
+      {
+        getNextPageParam: (lastPage, allPages) => {
+          const nextPage = allPages.length + 1;
+          return nextPage;
+        },
+      }
+    );
 
   return (
     <div className="App">
       {isSuccess &&
-        data.items.map((comment) => (
-          <div>
-            <span>{comment.name}</span>
-            <p>{comment.description}</p>
-          </div>
-        ))}
+        data.pages.map((page) =>
+          page.items.map((comment) => (
+            <div className="result" key={comment.id}>
+              <span>{comment.name}</span>
+              <p>{comment.description}</p>
+            </div>
+          ))
+        )}
     </div>
   );
 }
